a22b6575972f0fe9e9e09cbd76396baeb354bc8c
Eyal Cohen <eyal88@gmail.com>
2016-03-10 12:44:18 +0200

    uthreads.h and main

---
+++ main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  os_ex2
+//
+//  Created by Eyal Cohen on 10/03/2016.
+//  Copyright © 2016 Eyal Cohen. All rights reserved.
+//
+
+#include "uthreads.h"
+
+int main(void)
+{
+    return 0;
+}
